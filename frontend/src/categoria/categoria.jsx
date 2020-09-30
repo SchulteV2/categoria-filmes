4eb87bfd686e0f6f4e9eb050221934c367119293
Vinicius Schulte <schultev2@gmail.com>
2020-09-30 17:49:09 -0300

    Criado os componentes categoria e filmes, chamando eles no app.jsx

---
+++ frontend/src/categoria/categoria.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Categoria extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Categoria</h1>
+            </div>
+        )
+    }
+}
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Categoria from '../categoria/categoria'
+import Filme from '../filme/filme'
 
 export default props => (
     <div className='container'>
-        <h1>Teste</h1>
+        <Categoria />
+        <Filme />
     </div>
 )
