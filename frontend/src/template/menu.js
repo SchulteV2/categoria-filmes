1fc1f95a3713d7a9b03523aa08ef637f58d0b962
Vinicius Schulte <schultev2@gmail.com>
2020-10-02 17:27:17 -0300

    correção de href

--- frontend/src/template/menu.js
+++ frontend/src/template/menu.js
@@ -5,10 +5,10 @@ export default props => (
         <nav className="navbar navbar-expand-sm bg-dark">
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <a className="nav-link text-decoration-none text-light" href="#/filmes">Filmes</a>
+                    <a className="nav-link text-decoration-none text-light" href="#/filme">Filmes</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link text-decoration-none text-light" href="#/categorias">Categorias</a>
+                    <a className="nav-link text-decoration-none text-light" href="#/categoria">Categorias</a>
                 </li>
             </ul>
         </nav>
