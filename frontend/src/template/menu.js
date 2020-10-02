75727c05775fd3428b1ee0b722b016f5ac4b07a2
Vinicius Schulte <schultev2@gmail.com>
2020-10-01 22:53:40 -0300

    correção na navbar

--- frontend/src/template/menu.js
+++ frontend/src/template/menu.js
@@ -2,20 +2,15 @@ import React from 'react'
 
 export default props => (
     <div>
-        <nav className='navbar navbar-inverse bg-inverse'>
-            <div className='container'>
-                <div className='navbar-header'>
-                    <a className='navbar-brand' href='/#'>
-                        <i className='fa fa-calendar-check-o'></i> CategoriaFilmes
-                    </a>
-                </div>
-                <div id='navbar' className='navbar-collapse collapse'>
-                    <ul className="navbar-nav">
-                        <li><a href='#/filmes'>Filmes</a></li>
-                        <li><a href='#/categorias'>Categorias</a></li>
-                    </ul>
-                </div>
-            </div>
+        <nav className="navbar navbar-expand-sm bg-dark">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link text-decoration-none text-light" href="#/filmes">Filmes</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-decoration-none text-light" href="#/categorias">Categorias</a>
+                </li>
+            </ul>
         </nav>
     </div>
 )
