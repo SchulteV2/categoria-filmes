8c1711cd5d75f4d698b8d796dc62ef77cbe820cf
Vinicius Schulte <schultev2@gmail.com>
2020-10-05 18:27:47 -0300

    Adicionado listagem do conteudo já inserido no banco e também adicionada função delete

--- frontend/src/categoria/categoriaList.js
+++ frontend/src/categoria/categoriaList.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+const URL = 'http://localhost:3003/api/categoria'
+
+export default () => {
+
+    const [nomes, setNome] = useState([]);
+
+    useEffect(() => {
+        const getNome = async () => {
+            const { data } = await axios.get(URL)
+            setNome(data)
+        }
+        getNome()
+
+    }, [])
+
+    const handleDelete = (_id) => {
+        axios.delete(`${URL}/${_id}`)
+            .then(resp => {
+                const del = nomes.filter(nome => _id !== nome._id)
+                window.location.reload()
+            }) 
+    }
+
+
+    return (
+        <div className="container">
+            <h1>Lista de Categorias de Filmes</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Categoria</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {nomes.map(category => (
+                        <>
+                            <tr>
+                                <td key={category._id} value={category._id}>
+                                    {category.nome}
+                                   
+                                </td>
+                                <td key={category._id} value={category._id}>
+                                    <IconButton styled='danger' icon='trash'
+                                        onClick={() => handleDelete(category._id)}></IconButton>
+                                </td>
+                            </tr>
+                        </>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- frontend/src/filme/filmeList.js
+++ frontend/src/filme/filmeList.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+const URL = 'http://localhost:3003/api/filme'
+
+export default () => {
+
+    const [nomes, setNome] = useState([]);
+    const [duracao, setDuracao] = useState([]);
+    const [categoriaId, setCategoriaId] = useState([]);
+
+    const handleDelete = (_id) => {
+        axios.delete(`${URL}/${_id}`)
+            .then(resp => {
+                const del = nomes.filter(nome => _id !== nome._id)
+                window.location.reload()
+            })
+    }
+
+    useEffect(() => {
+        const getNome = async () => {
+            const { data } = await axios.get(URL)
+            setNome(data)
+            setDuracao(data)
+            setCategoriaId(data)
+        }
+        getNome()
+    }, [])
+
+    return(
+        <div className="container">
+            <h1>Lista de Filmes</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Filme</th>
+                        <th>Duração</th>
+                        <th>Categoria</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {nomes.map(nome => (
+                        <>
+                            <tr>
+                                <td key={nome._id} value={nome._id}>{nome.nome}</td>
+                                <td key={nome._id} value={nome._id}>{nome.duracao}</td>
+                                <td key={nome._id} value={nome._id}>{nome.categoriaId}</td>
+                                <td key={nome._id} value={nome._id}>
+                                    <IconButton styled='danger' icon='trash'
+                                        onClick={() => handleDelete(nome._id)}></IconButton>
+                                </td>
+
+                            </tr>
+                        </>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
